3f49504b3fe79fce7270e7d9de00c535a42e7d73
sharvilk <sk@pray.com>
2017-11-25 11:06:28 -0800

    enroll as

--- qa.py
+++ qa.py
@@ -1,25 +1,37 @@
 from os import path
+import os
 import json
 
 def enroll_answer(question_id, answer, speaker_id, speaker_name):
-	a[question_id] =  
-	fullpath = '/answers/'+speaker_name+'_ans.json'
+	apath = '/home/sharvil/deepLearning/projec/python/answers/'
+	filename = speaker_name+'_ans.json'
+	fullpath = apath + filename
+	a = {}
+	if not path.exists(apath):
+		os.makedirs(apath)
 	if not path.isfile(fullpath):
-		a.append(entry)
-		with open(fullpath, mode='w') as f:
-			f.write(json.dumps(entry, indent=2))
+		with open(fullpath, mode='w') as a_json:
+			a[question_id] = answer
+			a_json.write(json.dumps(a, indent=2))
 	else:
-		with open(fullpath, 'r') as a_json:
-		    ids = json.load(a_json)
+		with open(fullpath, mode='r') as a_json:
+			answers = json.load(a_json)
+		answers[question_id] = answer
+		with open(fullpath, mode='w') as a_json:
+			a_json.write(json.dumps(answers, indent=2))
 
 
 def add_question(new_question):
 	with open('questions.json', mode='r') as q_json:
 		questions = json.load(q_json)
 	no_of_questions = len(questions)
-	questions[int(no_of_questions+1)] = new_question
+	questions[no_of_questions+1] = new_question
 	with open('questions.json', mode='w') as q_json:
 		q_json.write(json.dumps(questions, indent=2))
 
+def get_answer(question_id, speaker_name)
+	
+
 if __name__ == '__main__':
 	#add_question("What is my schedule")
+	#enroll_answer(1,"Watch the movie U.N.C.L.E.",1,'Vritvij')
