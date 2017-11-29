2aa0392dfa3430c55f26648a1f35c7d987a78783
Vritvij Kadam <vkadam@usc.edu>
2017-11-29 14:29:24 -0800

    Working UI

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false});
+  mainWindow = new BrowserWindow({width: 800, height: 600/*, frame: false*/});
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
